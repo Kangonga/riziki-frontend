c02ecb01a38e7475756e70d2db8391e3f26a16d2
unknown <guchu413@gmail.com>
2022-12-31 11:57:18 +0300

    Add and style footer

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -42,7 +42,26 @@ export default function LandingPage(){
                 </section>
             </main>
             <footer className="landingPageFooter">
+                <section className="footerDiv">
+                        <h1>For Talent</h1>
+                        <div>How to get hired</div>
+                        <div>Payment Platforms</div>
+                        <div>Conflict resolution</div>
+                </section>
 
+                <section className="footerDiv">
+                        <h1>For Employers</h1>
+                        <div>How to find talent</div>
+                        <div>Payment Platforms</div>
+                        <div>Conflict resolution</div>
+                </section>
+
+                <section className="footerDiv">
+                        <h1>Resources</h1>
+                        <div>Success Stories</div>
+                        <div>Blog</div>
+                        <div>Help and Support</div>
+                </section>
             </footer>
            
         </div>

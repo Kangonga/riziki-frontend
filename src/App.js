dd0b5cb9d3745abeccfabe4e8a2d4c7cbf959df4
Kangonga <guchu413@gmail.com>
2023-01-05 08:25:01 +0300

    create job form file and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom"
 import Employers from "./components/Employers";
 import LandingPage from "./components/landingPage";
+import AddJob from "./components/AddJob"
 import './employers.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <Routes>
      <Route path="/dashboard" element={<LandingPage />} />
      <Route path="" element={<Employers />} />
+     <Route path="/jobform" element={<Employers />} />
       {/* 
       <Route path="/users" element={<Users/>} />
       <Route path="/login" element={<Login/>} />

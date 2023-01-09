d5c1a8f3b8c2afaba8bbacf98cb7742fdfd0a6da
Kangonga <guchu413@gmail.com>
2023-01-09 15:12:44 +0300

    add job list page

--- src/components/jobs.jsx
+++ src/components/jobs.jsx
@@ -1,15 +1,21 @@
+import NavBar from "./NavBar"
+
 export default function Jobs(){
     return(
+        <main id="jobsPage">
+        <NavBar></NavBar>
         <section id="cardContainer">
                <Job /> 
                <Job /> <Job /> 
         </section>
+        </main>
+        
     )
 }
 function Job(){
     return(
         <>
-        <div className="card">
+        <div id="jobsCard" className="card">
                     <section className="jobDetails">
                     <h1>Job Title</h1>
                     <h2>Job Category</h2>
@@ -24,6 +30,7 @@ function Job(){
                             <li>React JS</li>
                         </ul>
                         <p>Job Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quam quod eligendi cum praesentium nesciunt voluptatum quos. Voluptate, modi repellat!</p>
+                        <button className="apply">Apply</button>
                         </section>
                 </div>
         </>

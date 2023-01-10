ab5c930d47683569c29220cf19906142b09405f1
Kangonga <guchu413@gmail.com>
2023-01-10 07:48:00 +0300

    fetch user successfuly

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,6 +35,14 @@ function LoginForm() {
     function handleSubmit(e){
       e.preventDefault()
       console.log(loginData)
+
+      fetch("http://127.0.0.1:3000/login", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(loginData)
+      })
+      .then((resp)=>resp.json())
+      .then(data=>console.log(data));
     }
     return(
       <div className="loginContainer">

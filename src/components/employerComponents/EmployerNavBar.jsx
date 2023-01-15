be3cb3f64c683b0cde044b3e68c833b990ccc26f
Kangonga <guchu413@gmail.com>
2023-01-15 14:38:23 +0300

    revert mefia queries for navbar

--- src/components/employerComponents/EmployerNavBar.jsx
+++ src/components/employerComponents/EmployerNavBar.jsx
@@ -5,26 +5,17 @@ import logo from "../../assets/sitelogo.jpg"
 
 export default function EmployerNavBar(){
     const {user,setUser} = useContext(UserContext)
-    const [showNav,setShowNav] = useState(false)
     function logOut(){
         setUser({})
-    }
-    function handleMenu(){
-        setShowNav(true)
     }
     return(
         <>
         {!user.username?<Navigate to="/login" />:null}
-        <div className="hamburgerLines" onClick={handleMenu}>
-            <span className="line line1"></span>
-            <span className="line line1"></span>
-            <span className="line line1"></span>
-        </div>
          <header className="header">
             <figure className="logoHolder">
                 <img src={logo} alt="sitelogo" className="sitelogo" />
             </figure>
-            <nav className={showNav?"showDiv":""}>
+            <nav>
             <Link to="/userlist">Find Talent</Link>
             <Link to="/jobform">Post a Job</Link>
             <Link to="/employerJobs">My Jobs</Link>

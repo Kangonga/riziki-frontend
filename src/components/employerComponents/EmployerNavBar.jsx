9ee5c505b74a86f0577e3f317961775e6cefb3a7
Kangonga <guchu413@gmail.com>
2023-01-15 14:29:56 +0300

    add media query for hero section

--- src/components/employerComponents/EmployerNavBar.jsx
+++ src/components/employerComponents/EmployerNavBar.jsx
@@ -1,26 +1,35 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Link,Navigate } from "react-router-dom"
 import { UserContext } from "../../App"
 import logo from "../../assets/sitelogo.jpg"
 
 export default function EmployerNavBar(){
     const {user,setUser} = useContext(UserContext)
+    const [showNav,setShowNav] = useState(false)
     function logOut(){
         setUser({})
+    }
+    function handleMenu(){
+        setShowNav(true)
     }
     return(
         <>
         {!user.username?<Navigate to="/login" />:null}
+        <div className="hamburgerLines" onClick={handleMenu}>
+            <span className="line line1"></span>
+            <span className="line line1"></span>
+            <span className="line line1"></span>
+        </div>
          <header className="header">
             <figure className="logoHolder">
                 <img src={logo} alt="sitelogo" className="sitelogo" />
             </figure>
-            <nav>
+            <nav className={showNav?"showDiv":""}>
             <Link to="/userlist">Find Talent</Link>
             <Link to="/jobform">Post a Job</Link>
             <Link to="/employerJobs">My Jobs</Link>
             <Link to="/profile">My Profile</Link>
-            <Link to="/login" id="login">{user.id?"Log Out":"Log In"}</Link>
+            <Link to="/login" onClick={logOut} id="login">{user.id?"Log Out":"Log In"}</Link>
             </nav>
             </header>
         </>

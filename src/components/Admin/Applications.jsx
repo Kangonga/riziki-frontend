5c3960ece19bdd558e4b60edcb378120cdfee8a2
mercylynn <mercy.kamene@student.moringaschool.com>
2023-01-16 10:19:02 +0300

    add a state

--- src/components/Admin/Applications.jsx
+++ src/components/Admin/Applications.jsx
@@ -4,25 +4,25 @@ import { useState } from "react";
 
 
 const columns = [
-  { field: 'id', headerName: 'User ID', width: 70 },
-  { field: 'jobseeker_id', headerName: 'User ID', width: 70 },
-   { field: 'category', headerName: 'Category', width: 300, 
-  valueGetter:(users)=> users.row.job.category},
-  { field: 'job_title', headerName: 'Job Title', width: 300, 
-  valueGetter:(users)=> users.row.job.job_title},
+  { field: 'id', headerName: 'UserID', width: 150 },
+  { field: 'jobseeker_id', headerName: 'JobSeekerID', width: 200 },
+   { field: 'category', headerName: 'Category', width: 200, 
+  valueGetter:(applications)=> applications.row.job.category},
+  { field: 'job_title', headerName: 'Job Title', width: 200, 
+  valueGetter:(applications)=> applications.row.job.job_title},
  
   
 ];
 
 
 const Applications = () => {
-  const [users,setUsers]= useState([]);
+  const [applications,setApplications]= useState([]);
   const [pageSize, setPageSize] = useState(10)
 
   useEffect(()=>{
     fetch('https://riziki.onrender.com/job_applications')
       .then(response => response.json())
-      .then((json)=> setUsers(json))
+      .then((applications)=> setApplications(applications))
      
   }, [])
 
@@ -31,10 +31,9 @@ const Applications = () => {
 
 
     <div style={{ width: '100%', height: 700 }}>
-  
-   
+      <h1>Applications</h1>
     <DataGrid
-    rows={users}
+    rows={applications}
     columns={columns}
     pageSize={pageSize}
     rowsPerPageOptions={[8,5,10]}

bd37a1ccb0d55f5aac22307701b35635f14e64db
mercylynn <mercy.kamene@student.moringaschool.com>
2023-01-16 10:25:15 +0300

    remove unused react-icons

--- src/components/Admin/SidebarData.jsx
+++ src/components/Admin/SidebarData.jsx
@@ -1,103 +1,10 @@
-import {
-    FaTh,
-    FaBars,
-    FaHome,
-}from "react-icons/fa";
+import {FaTh,FaHome,}from "react-icons/fa";
 import {BsPeopleFill, BsClipboardCheck} from 'react-icons/bs'
 import {MdWork, MdLogout} from 'react-icons/md'
 
-
-// import * as FaIcons from "react-icons/fa";
-// import * as FcIcons from "react-icons/fc";
-// import * as MdIcons from "react-icons/md";
-// import * as MdWork from "react-icons/md";
-// import * as AiIcons from "react-icons/ai";
-// import * as FaUsers from "react-icons/fa";
-// import * as AiFillCheckSquare from "react-icons/ai"
-// import * as CgIcons from "react-icons/cg";
-// import {BiBus} from "react-icons/tfi";
-
-
-
-
-//     {
-//         title: "Home",
-//         path: "/",
-//         icon: <FaIcons.FaHome />,
-//       },
-//       // {
-//       //   title: "Dashboard",
-//       //   path: "/mainpage/dashboard",
-//       //   icon: <MdIcons.MdDashboard />,
-//       // },
-   
-//       // {
-//       //   title: "Users",
-//       //   path: "/mainpage/bookings",
-//       //   icon: <FaIcons.FaUsers />,
-//       // },
-//       // {
-//       //   title: "Employers",
-//       //   path: "/mainpage/buses",
-//       //   icon:  <FaIcons.FaUsers />,
-//       // },
-//       // {
-//       //   title: "JobSeekers",
-//       //   path: "/mainpage/drivers",
-//       //   icon:  <FaIcons.FaUsers />,
-//       // },
-//       // {
-//       //   title: "Applications",
-//       //   path: "/mainpage/customers",
-//       //   icon: <AiIcons.AiFillCheckSquare />,
-//       // },
-//       // {
-//       //   title: "Jobs",
-//       //   path: "/routes",
-//       //   icon: <MdIcons.MdWork />,
-//       // },
-//        {
-//         title: "Employers",
-//         path: "mainpage/employers",
-//         icon: <MdIcons.MdWork />,
-//       },
-//       {
-//         title: "Jobs",
-//         path: "/mainpage/jobs",
-//         icon: <MdIcons.MdWork />,
-//       },
-
-//       {
-//         title: "JobSeekers",
-//         path: "/mainpage/jobseekers",
-//         icon: <MdIcons.MdWork />,
-//       },
-//       {
-//         title: "Application",
-//         path: "/mainpage/applications",
-//         icon: <MdIcons.MdWork />,
-//       },
-//       {
-//         title: "Application",
-//         path: "/mainpage/jobseeker",
-//         icon: <MdIcons.MdWork />,
-//       },
-      
-//       // {
-//       //   title: "Profile",
-//       //   path: "/profile",
-//       //   icon: <CgIcons.CgProfile />,
-//       // },
-//       {
-//         title: "Logout",
-//         path: "/logout",
-//         icon: <MdIcons. MdLogout />,
-//       },
-      
-// ]
 export const SidebarData=[
         {
-            path:"/mainpage",
+            path:"/admin",
             title:"Dashboard",
             icon:<FaTh/>
         },
@@ -107,30 +14,26 @@ export const SidebarData=[
             icon:<FaHome/>
         },
         {
-            path:"/mainpage/employers",
+            path:"/admin/employers",
             title:"Employers",
             icon:<BsPeopleFill/>
         },
         {
-            path:"/mainpage/jobseekers",
+            path:"/admin/jobseekers",
             title:"JobSeekers",
             icon:<BsPeopleFill/>
         },
         {
-            path:"/mainpage/jobs",
+            path:"/admin/jobs",
             title:"Jobs",
             icon:<MdWork/>
         },
         {
-            path:"/mainpage/applications",
+            path:"/admin/applications",
             title:"Applications",
             icon:<BsClipboardCheck/>
         },
-        {
-            path:"/mainpage/jobseeker",
-            title:"Jobs",
-            icon:<BsClipboardCheck/>
-        },
+        
         {
             path:"/logout",
             title:"Logout",

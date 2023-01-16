f35f8074ea3b4ff95df1e4842aac3187253c832e
mercylynn <mercy.kamene@student.moringaschool.com>
2023-01-16 10:26:13 +0300

    fix link path

--- src/components/Admin/Navbar.jsx
+++ src/components/Admin/Navbar.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {FaBars} from 'react-icons/fa';
+import { FaBars } from "react-icons/fa";
 function Navbar() {
   return (
     <>
-      <div className='nav-bar'>
-        <div className='menu-bar'>
-          <Link to="/mainpage">
+      <div className="nav-bar">
+        <div className="menu-bar">
+          <Link to="/admin">
             <FaBars />
           </Link>
-           
         </div>
         <div>
-          <p className='title-admin'>Admin</p>
+          <p className="title-admin">Admin</p>
         </div>
       </div>
     </>
-  )
+  );
 }
 export default Navbar;

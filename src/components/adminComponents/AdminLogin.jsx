72b86d075594d476e5a945fab97ba1c8754528d7
Kangonga <guchu413@gmail.com>
2023-01-17 08:07:58 +0300

    add correct route to admin login

--- src/components/adminComponents/AdminLogin.jsx
+++ src/components/adminComponents/AdminLogin.jsx
@@ -41,7 +41,7 @@ function LoginForm() {
   function handleSubmit(e){
     e.preventDefault()
 
-    fetch("http://127.0.0.1:3000/login", {
+    fetch("http://127.0.0.1:3000/adminLogin", {
       method: "POST",
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify(loginData)
